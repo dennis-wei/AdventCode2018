ab821c7b606a2df65e51fce9d97299c83babb3d7
Dennis Wei <dwei@atlassian.com>
2018-12-01 20:28:34 -0800

    actual solution

---
+++ 1-CronalCalibration/solution.py
@@ -0,0 +1,17 @@
+with open('input.txt', 'r') as f:
+    nums = [int(l.strip()) for l in f]
+
+print(f'Part 1 solution is {sum(nums)}')
+
+curr = 0
+curr_index = 0
+reached = set()
+
+while True:
+    curr += nums[curr_index]
+    if curr in reached:
+        print(f'Part 2 solution is {curr}')
+        break
+    else:
+        reached.add(curr)
+        curr_index = (curr_index + 1) % len(nums)
